d228031218c3036442c3dbb15859211d5f702e7a
segasi <segasi@gmail.com>
2018-10-21 11:30:14 -0500

    Cálculo del porcentaje acumulado de municipios que a pesar de formar parte del subuniverso que conecentra el 80% de la población nacional, no se instalará una casilla de votación en la consulta

--- 02_codigo/analisis_mpos_consulta_naicm.R
+++ 02_codigo/analisis_mpos_consulta_naicm.R
@@ -342,3 +342,25 @@ bd %>%
 ggsave(filename = "mpos_del_80_porciento_mas_poblado_no_incluidos.png", path = "03_graficas/", width = 15, height = 10, dpi = 100)
 
 
+### Cálculo del porcentaje acumulado de municipios que a pesar de formar parte del subuniverso que conecentra el 80% de la población nacional, no se instalará una casilla de votación en la consulta ----
+
+bd %>% 
+  arrange(-pob_tot) %>% 
+  mutate(pob_acumulada = cumsum(pob_tot),
+         pob_tot_nal = sum(pob_tot),
+         por_pob_acumulada = round((pob_acumulada/pob_tot_nal)*100, 5),
+         mpo_incluido = ifelse(!is.na(municipios_nom), "Sí", "No")) %>% 
+  select(edo_nom, mpo_nom, 
+         pob_tot, 
+         pob_acumulada, 
+         pob_tot_nal, 
+         por_pob_acumulada, 
+         mpo_incluido) %>% 
+  filter(por_pob_acumulada <=80,
+         mpo_incluido == "No") %>% 
+  group_by(edo_nom) %>%
+  summarise(num_mpos = n()) %>% 
+  ungroup() %>% 
+  arrange(-num_mpos) %>% 
+  mutate(por_mpos_cum = round((cumsum(num_mpos)/sum(num_mpos))*100, 1)) %>% 
+  print(n = Inf)
